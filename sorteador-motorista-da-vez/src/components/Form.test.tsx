dfea9f600cd2b26876d0fb2315cb4c4141ff8dc8
barbara santos <barbaransantos1@gmail.com>
2023-02-20 12:00:18 -0300

    creted the test insertion name

--- sorteador-motorista-da-vez/src/components/Form.test.tsx
+++ sorteador-motorista-da-vez/src/components/Form.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Form } from "./Form";
 
 test('If o input is not input, new users can not be added', () => {
@@ -20,4 +20,17 @@ test('Add a participant when a name is called', () => {
   )
   const input = screen.getByPlaceholderText('Enter participant names')
   const button = screen.getByRole('button')
-)
+
+  // insert a input value
+  fireEvent.change(input, {
+    target: {
+      value: 'Ana Beatriz'
+    }
+  })
+  // click a submit button
+  fireEvent.click(button)
+  // garantee the focus active
+  expect(input).toHaveFocus()
+  //guarantee the input will be clean
+  expect(input).toHaveValue('')
+})
--- sorteador-motorista-da-vez/src/components/Form.tsx
+++ sorteador-motorista-da-vez/src/components/Form.tsx
@@ -1,12 +1,27 @@
+import { useRef, useState } from "react"
 
 
 export function Form() {
+
+  const [name, setName] = useState('')
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const AddName = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setName('')
+    inputRef.current?.focus()
+  }
+
   return (
-    <form action="">
+    <form onSubmit={AddName}>
       <input
-        type=""
+        ref={inputRef}
+        value={name}
+        onChange={event => setName(event.target.value)}
+        type="text"
         placeholder="Enter participant names"/>
-      <button disabled={true}>Adicionar</button>
+      <button disabled={!name}>Adicionar</button>
     </form>
   )
 }
